d79265bdb54e01429d97e69b5d736881a2b58f41
kanekotic <alvarojosepl@gmail.com>
2019-03-30 10:28:22 +0100

    ongoing changes

--- lib/index.ts
+++ lib/index.ts
@@ -2,25 +2,31 @@ import { Request, Response, Router } from 'express'
 import { ActorSystem } from 'tarant'
 
 const SyncController = (system: ActorSystem, config: any): Router => {
+  console.log(config.actorTypes)
   const router: Router = Router()
 
   router.get(`${config.paths.pull}/:id`, async (req: Request, res: Response) => {
+    console.log('get', req.params.id)
     try {
       const actor = (await system.actorFor(req.params.id)) as any
       res.json(await actor.toJson())
+      console.log('get result', actor)
     } catch (_) {
+      console.log('get error')
       res.sendStatus(404)
     }
   })
 
   router.post(`${config.paths.push}/:id`, async (req: Request, res: Response) => {
-    let actor
+    let actor: any
     try {
-      actor = (await system.actorFor(req.params.id)) as any
+      actor = await system.actorFor(req.params.id)
     } catch (_) {
-      actor = (await system.actorOf(config.actorTypes[req.body.type], [req.params.id])) as any
+      actor = await system.actorOf(config.actorTypes[req.body.type], [req.params.id])
     }
+    console.log('here', actor, (actor as any).updateFrom) 
     actor.updateFrom(req.body)
+    console.log('here2')
     res.sendStatus(200)
   })
 
