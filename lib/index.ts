865314202ff4f58ea96c3934b98c95ecd70d8d1f
kanekotic <me@kanekotic.com>
2019-11-30 13:22:42 -0600

    improve the code by using resolve or new

--- lib/index.ts
+++ lib/index.ts
@@ -2,32 +2,26 @@ import { Request, Response, Router } from 'express'
 import { ActorSystem } from 'tarant'
 
 const SyncController = (system: ActorSystem, config: any): Router => {
-  console.log(config.actorTypes)
   const router: Router = Router()
 
   router.get(`${config.paths.pull}/:id`, async (req: Request, res: Response) => {
-    console.log('get', req.params.id)
     try {
       const actor = (await system.actorFor(req.params.id)) as any
       res.json(await actor.toJson())
-      console.log('get result', actor)
     } catch (_) {
-      console.log('get error')
       res.sendStatus(404)
     }
   })
 
   router.post(`${config.paths.push}/:id`, async (req: Request, res: Response) => {
-    let actor: any
     try {
-      actor = await system.actorFor(req.params.id)
-    } catch (_) {
-      actor = await system.actorOf(config.actorTypes[req.body.type], [req.params.id])
+      let actor: any = await system.resolveOrNew(req.params.id, config.actorTypes[req.body.type], [req.params.id])
+      actor.updateFrom(req.body)
+      res.sendStatus(200)
+    } catch (error) {
+      res.send(500)
     }
-    console.log('here', actor, (actor as any).updateFrom) 
-    actor.updateFrom(req.body)
-    console.log('here2')
-    res.sendStatus(200)
+
   })
 
   return router
